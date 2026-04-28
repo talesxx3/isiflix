a33f057789bb0d0a54abf9375a66a7e6b46bab86
talesxx3 <tales_silva2013@live.com>
2026-04-28 20:13:47 -0300

    feat: pacotes e modificador de acesso protected

---
+++ src/core/Produto.java
@@ -0,0 +1,38 @@
+package core;
+
+public class Produto {
+    private int id;
+    private String descricao;
+    private int preco;
+
+    public Produto(int id, String descricao, int preco) {
+        this.id = id;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getPreco() {
+        return preco;
+    }
+
+    public void setPreco(int preco) {
+        this.preco = preco;
+    }
+
+}
---
+++ src/userinterface/MainClass.java
@@ -0,0 +1,11 @@
+package userinterface;
+
+import core.Produto;
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("TESTANDO PACOTE");
+
+        Produto p = new Produto(1, "Celular", 5000);
+    }
+}
